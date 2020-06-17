15d4d7e9a2a7201173f9404c47e1456d92bdc3de
LEE026 <58213749+LEE026@users.noreply.github.com>
2020-06-17 17:02:04 +0900

    포탈 수정

--- game/data/obj/potal.py
+++ game/data/obj/potal.py
@@ -3,7 +3,7 @@ size=40
 '''텔레포트 될곳에 객체가 있을경우 겹쳐지는 오류발생'''
 
 class subpotal(pygame.sprite.Sprite):
-    def __init__(self, img, location, area, obj,potal):  # 이미지,설치좌표(튜플로 전달),넓이와 높이를 튜플로 전달
+    def __init__(self, img, location, area, obj,potal,stopframe,term):  # 이미지,설치좌표(튜플로 전달),넓이와 높이를 튜플로 전달
         pygame.sprite.Sprite.__init__(self)  # 스프라이트 초기화
         self.image = pygame.transform.scale(img, area)  # 이미지의 크기를 내가 원하는 크기로 조정
         self.rect = self.image.get_rect()  # 이미지의 사각형에 해당하는 범위를 가져옴
@@ -12,20 +12,17 @@ class subpotal(pygame.sprite.Sprite):
 
         self.potal=potal
         self.col_obj=obj
+        self.stopframe=stopframe
+        self.term=term
+        self.a=0
 
-    def teleport(self,ball):
-        if(pygame.sprite.collide_mask(ball,self)):
-            loc=list(self.potal.rect.center)
-            #어디서 충ehf했는지 확인
-            if ball.get_center(0)<self.rect.left:
-                loc[0]+=size
-            elif ball.get_center(0)>self.rect.right:
-                loc[0] -= size
-            if ball.get_center(1)<self.rect.top:
-                loc[1]+=size
-            elif ball.get_center(1)>self.rect.bottom:
-                loc[1] -= size
-            ball.set_location(loc)
+    def teleport(self,ball,s):
+
+
+        if self.stopframe[0] == 0: #sub에서는 차감 x
+            if(pygame.sprite.collide_mask(ball,self)):
+                self.stopframe[0] = self.term
+                ball.set_location(self.potal.rect.center)
 
         for obj in self.col_obj:
             if (pygame.sprite.collide_mask(obj, self)):
@@ -43,7 +40,7 @@ class subpotal(pygame.sprite.Sprite):
 
 
 class Potal(pygame.sprite.Sprite):
-    def __init__(self, img1,img2, location1,location2, area,obj=[],group=[]):
+    def __init__(self, img1,img2, location1,location2, area,FPS=60,time=2,obj=[],group=[]):
         '''블럭 이미지1,2, 위치(튜플)1,2,면적(튜플),충돌 가능성이 있는 객체들 공제외 (리스트),충돌가능성이 있는 그룹들 (리스트)'''
         pygame.sprite.Sprite.__init__(self)  # 스프라이트 초기화
         self.image = pygame.transform.scale(img1, area)  # 이미지의 크기를 내가 원하는 크기로 조정
@@ -51,30 +48,25 @@ class Potal(pygame.sprite.Sprite):
         self.rect.topleft = location1  # 위치설정
         self.mask = pygame.mask.from_surface(self.image)  # 충돌감지를 위한 마스크생성
 
-        self.potal=subpotal(img2,location2,area,obj,self)
+
+        self.stopframe=[0]
+        self.term=FPS*time
+        self.potal=subpotal(img2,location2,area,obj,self,self.stopframe,self.term)
         self.col_obj=obj
         self.col_group=group
 
-    def return_subpotal(self):
-        return self.potal
 
     def draw_portal(self,background):
         background.blit(self.potal.image,self.potal.rect)
 
     def teleport(self,ball):
         '''이동할 곳에  다른 객체가 존재할 경우 겹쳐지는 문제 발생'''
-        if(pygame.sprite.collide_mask(ball,self)):
-            loc=list(self.potal.rect.center)
-            #어디서 충ehf했는지 확인
-            if ball.get_center(0)<self.rect.left:
-                loc[0]+=size
-            elif ball.get_center(0)>self.rect.right:
-                loc[0] -= size
-            if ball.get_center(1)<self.rect.top:
-                loc[1]+=size
-            elif ball.get_center(1)>self.rect.bottom:
-                loc[1] -= size
-            ball.set_location(loc)
+        if self.stopframe[0]==0:
+            if(pygame.sprite.collide_mask(ball,self)):
+                self.stopframe[0]=self.term
+                ball.set_location(self.potal.rect.center)
+        else:
+            self.stopframe[0]-=1
 
         for obj in self.col_obj:
             if (pygame.sprite.collide_mask(obj, self)):
@@ -104,6 +96,8 @@ class Potal(pygame.sprite.Sprite):
                 elif obj.get_center(1) > self.rect.bottom:
                     loc[1] -= size
                 obj.set_location(loc)
+
+        self.potal.teleport(ball,self.stopframe)
 
     def set_collision_obj(self,obj_list):
         self.col_obj=obj_list
