70631ad62f04ca66d6f64d88e7ee5af7f85918e2
이혁수 <lhs456852@gmail.com>
2020-06-18 21:45:02 +0900

    추가 및 변경사항 - mapediter 일부 수정 후 업로드 - LoadMap 모든 객체 사용 가능 - LoadMap - map.txt UTF-8로 인코딩 -
    삭제
    - mapediter.zip 삭제
    -

---
+++ game/data/obj/Setting.py
@@ -0,0 +1,15 @@
+class set():
+    def __init__(self):
+        self.width=1920
+        self.height=1080
+        self.size=40
+        self.FPS = 30
+        self.Fall_speed=4
+        self.wall=None
+        #self.time_adjustment=60/self.FPS
+
+    def set_FPS(self,FPS):
+        self.FPS=FPS
+        #self.time_adjustment=60/self.FPS
+
+setting=set()
